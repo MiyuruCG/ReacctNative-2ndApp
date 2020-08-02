e744070bafb9d2359ff0d6d4226f088dd0080803
MiyuruCG <miyurugunatilake@gmail.com>
2020-08-02 14:32:20 +0530

    styling the game screen

--- screen/StartGameScreen.js
+++ screen/StartGameScreen.js
@@ -1,10 +1,18 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, TextInput, Button } from "react-native";
 
 const StartGameScreen = (props) => {
   return (
     <View style={styles.screen}>
-      <Text>Game screen</Text>
+      <Text>Start a new Game</Text>
+      <View style={styles.inputContainer}>
+        <Text style={styles.title}>Select a Number</Text>
+        <TextInput />
+        <View style={styles.buttonsContainer}>
+          <Button title="Reset" onPress={() => {}} />
+          <Button title="Confirm" onPress={() => {}} />
+        </View>
+      </View>
     </View>
   );
 };
@@ -14,6 +22,21 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 10,
     alignItems: "center",
+  },
+  title: {
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  inputContainer: {
+    width: 300,
+    maxWidth: "80%",
+    alignItems: "center",
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+    paddingHorizontal: 15,
   },
 });
 
