d152119ef7123b1807ae12d738bb4e8cd7af9801
MiyuruCG <miyurugunatilake@gmail.com>
2020-08-02 14:15:28 +0530

    create the header component

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, View } from "react-native";
+import Header from "./componetns/Header";
 
 export default function App() {
   return (
-    <View>
+    <View style={styles.screen}>
+      <Header title="Guess Number" />
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
+});
